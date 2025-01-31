7bfd9cdff28d2bc289378953c0357fa44666424e
Anika Bose <arbose@usc.edu>
2025-01-30 18:13:42 -0800

    All done with Part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,42 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
-/* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // base case, exit once singly-linked list "in" is empty
+  if (in == NULL){
+    return;
+  }
+
+  // save next node in "in" list so that we do not lose track of list
+  Node* nextElement = in -> next;
+
+  // mod % 2 to check if even or odd
+  if (in -> value % 2 == 0) {
+    // disconnect node from in list by setting next value to null. Set current node of evens to current/ head node of in
+    in -> next = NULL;  
+    evens = in;
+
+    // recursively call function split(), moving onto next element in list "evens" because 
+    // current head of evens now points to current element/ head of ins 
+    split(nextElement, odds, evens -> next);
+  } else {
+    // disconnect node from in list by setting next value to null. Set current node of evens to current/ head node of in
+    in -> next = NULL;
+    odds = in;
+
+    // recursively call function split(), moving onto next element in list "odds" because 
+    // current head of odds now points to current element/ head of ins 
+    split(nextElement, odds -> next, evens);
+  } 
+
+  // ensures in list is empty and set to null 
+  in = NULL;
 }
 
-/* If you needed a helper function, write it here */
+
+
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,105 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+// prototypes
+void printList(Node* head);
+void deleteList(Node* head);
 
 int main(int argc, char* argv[])
 {
+    // test case: no values in "in"
+    Node* odds = NULL;
+    Node* evens = NULL;
+    Node* list1 = NULL;
+    cout << "Before: ";
+    printList(list1);
+    split(list1, odds, evens);
+    cout << "Odds test 1: ";
+    printList(odds);
+    cout << "Evens test 1: ";
+    printList(evens);
+    cout << "Ins test 1: ";
+    printList(list1);
+    deleteList(odds);
+    deleteList(evens);
+    deleteList(list1);  
+    list1 = NULL;
+    
+    // test case: only evens
+    odds = NULL;
+    evens = NULL;
+    Node* list2 = new Node(2, new Node(4, new Node(6, NULL)));;
+    cout << "Before: ";
+    printList(list2);
+    split(list2, odds, evens);
+    cout << "Odds test 2: ";
+    printList(odds);
+    cout << "Evens test 2: ";
+    printList(evens);
+    cout << "Ins test 2: ";
+    printList(list2);
+    deleteList(odds);
+    deleteList(evens);
+    deleteList(list2);
+    list2 = NULL;
 
+    // test case: single element, odd
+    odds = NULL;
+    evens = NULL;
+    Node* list3 = new Node(5, NULL);
+    cout << "Before: ";
+    printList(list3);
+    split(list3, odds, evens);
+    cout << "Odds test 3: ";
+    printList(odds);
+    cout << "Evens test 3: ";
+    printList(evens);
+    cout << "Ins test 3: ";
+    printList(list3);
+    deleteList(odds);
+    deleteList(evens);
+    deleteList(list3);
+    list3 = NULL;
+
+    // test case: single element, even
+    odds = NULL;
+    evens = NULL;
+    Node* list4 = new Node(8, NULL);
+    cout << "Before: ";
+    printList(list4);
+    split(list4, odds, evens);
+    cout << "Odds test 4: ";
+    printList(odds);
+    cout << "Evens test 4: ";
+    printList(evens);
+    cout << "Ins test 4: ";
+    printList(list4);
+    deleteList(odds);
+    deleteList(evens);
+    deleteList(list4);
+    list4 = NULL;
+}
+
+
+// print list for testing
+void printList(Node* head) {
+    while (head != NULL) {
+        cout << head -> value << " ";
+        head = head -> next;
+    }
+    cout << endl;
+}
+
+// allocated lists for testing, this now deletes from memory
+void deleteList(Node* head) {
+	Node* current = head;
+	while(current != NULL){
+		Node* next = current -> next;
+		delete current;
+		current = next;
+	}
 }
